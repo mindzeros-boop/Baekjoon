da7c539e8db747e7eb40390cecfcb10f010da2e4
mindzeros <mindzeros@icloud.com>
2026-03-21 01:13:22 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 72 ms, Memory: 5412 KB -BaekjoonHub

---
+++ C#/백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.cs
@@ -0,0 +1,21 @@
+while (true)
+{
+    string str = Console.ReadLine();
+
+    if (str == "end")
+    {
+        break;
+    }
+    if (str == "flower")
+    {
+        Console.WriteLine("Forsythia koreana");
+    }
+    if (str == "animal")
+    {
+        Console.WriteLine("Panthera tigris");
+    }
+    if (str == "tree")
+    {
+        Console.WriteLine("Pinus densiflora");
+    }
+}
