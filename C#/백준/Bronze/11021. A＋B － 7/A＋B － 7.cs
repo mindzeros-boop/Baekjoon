25e62ff3811620c3cdbedc7c684b18f7bed010f2
mindzeros <mindzeros@icloud.com>
2026-03-21 12:21:33 +0900

    [Bronze V] Title: A+B - 7, Time: 48 ms, Memory: 5912 KB -BaekjoonHub

---
+++ C#/백준/Bronze/11021. A＋B － 7/A＋B － 7.cs
@@ -0,0 +1,8 @@
+int num = int.Parse(Console.ReadLine());
+for (int i = 0; i < num; i++)
+{
+    string[] a = Console.ReadLine().Split();
+    int[] input = new int[2];
+    input = Array.ConvertAll(a, int.Parse);
+    Console.WriteLine($"Case #{i + 1}: {input[0] + input[1]}");
+}
