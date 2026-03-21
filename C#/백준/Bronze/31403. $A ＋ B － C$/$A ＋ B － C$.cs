d7cebffcf5c39bdd466dac61fa2d130fb0898f87
mindzeros <mindzeros@icloud.com>
2026-03-21 12:17:21 +0900

    [Bronze IV] Title: $A + B - C$, Time: 48 ms, Memory: 5488 KB -BaekjoonHub

---
+++ C#/백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cs
@@ -0,0 +1,8 @@
+string A = Console.ReadLine();
+string B = Console.ReadLine();
+int C = int.Parse(Console.ReadLine());
+string D = A + B;
+int output = int.Parse(D) - C;
+
+Console.WriteLine(int.Parse(A) + int.Parse(B) - C);
+Console.WriteLine(output);
