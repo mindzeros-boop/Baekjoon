95686d8aeeeadff1dde442f3412b4890402ffcae
mindzeros <mindzeros@icloud.com>
2026-03-21 12:23:29 +0900

    [Bronze V] Title: 사칙연산, Time: 48 ms, Memory: 5500 KB -BaekjoonHub

---
+++ C#/백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,9 @@
+string input = Console.ReadLine();
+string[] numbers = input.Split(' ');
+int a = int.Parse(numbers[0]);
+int b = int.Parse(numbers[1]);
+Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+Console.WriteLine(a * b);
+Console.WriteLine(a / b);
+Console.WriteLine(a % b);
